1702a03833f7c632aea0b0db5fa733a394d6cf2a
Sujata34 <sujatamrane34@gmail.com>
2021-12-12 21:48:46 +0530

    files r added

---
+++ var.java
@@ -0,0 +1,18 @@
+class var
+{
+  int a=98;
+ String s;
+  public void add()
+	{
+  	System.out.println(a);
+         int a=10;
+	  System.out.println(a);
+	}
+ 
+ public static void main(String []hdsd)
+ {
+	var d = new var();
+	d.add();
+	
+  }
+}
